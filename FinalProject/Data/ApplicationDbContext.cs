98776a98aa8f48801166ab9111e4b7b6f79c0164
dk545617 <dk545617@users.noreply.github.com>
2020-12-13 10:21:41 -0600

    New Project

--- FinalProject/Data/ApplicationDbContext.cs
+++ FinalProject/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FinalProject.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,8 @@ namespace FinalProject.Data
             : base(options)
         {
         }
+
+        public DbSet<Menu> MenuItems { get; set; }
+
     }
 }
---
+++ FinalProject/Models/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        [Required (ErrorMessage ="You must enter a name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must enter a price")]
+        public int Price { get; set; }  
+
+    }
+}
